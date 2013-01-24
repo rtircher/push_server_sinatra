eb213a6112eaa8a9fe90ab9a35e71933e414df7e
Renaud Tircher <renaud.tircher@gmail.com>
2013-01-24 10:13:51 +0800

    Using Mongo to store the device tokens

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
 require 'apns'
+require 'mongo'
 
-@@users = {}
+include Mongo
+
+@@client = MongoClient.new('localhost', 27017)
+@@db     = @@client['device-db']
+@@tokens = @@db['tokens']
+@@tokens.create_index("user_id")
 
 ###################
 # Hosts Config
@@ -45,10 +51,11 @@ APNS.pem  = 'tools/development.pem'
 # (default: false)
 # APNS.cache_connections = true
 
-get '/users/:id/notify' do |user_id|
-  return "User #{user_id} not found" unless users[user_id]
+get '/users/:id/notifications' do |user_id|
+  user = @@tokens.find_one( "user_id" => user_id )
+  return "User #{user_id} not found" unless user
 
-  device_token = users[user_id][:token]
+  device_token = user["tokens"]
 
   if device_token
     # Single notification
@@ -66,14 +73,11 @@ get '/users/:id/notify' do |user_id|
   end
 end
 
-post '/users/:id/registerDevice' do |user_id|
-  users[user_id] = { :token => params[:token] } # need to have a set of token and add token ids when requested
+post '/users/:id/devices' do |user_id|
+  @@tokens.remove({ "user_id" => user_id })
+  @@tokens.insert({ "user_id" => user_id, "tokens" => params[:token] }) # need to have a set of token and add token ids when requested
+
+  @@tokens.find.each { |tokens| p tokens }
 
   status(200)
-end
-
-private
-
-def users
-  @@users
 end
